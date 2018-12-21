edad475a162362d97d8f6ce4048d4703ce2721b5
Arash Ghamkhari <dev@ghamkhari.com>
2018-12-21 16:10:34 +0330

    Opt added

---
+++ src/Opt.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of Opt.
+ *
+ * (c) Arash Ghamkhari <dev@ghamkhari.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Opt;
+
+class Opt implements OptInterface
+{
+    use OptTrait;
+}
---
+++ src/OptTrait.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of Opt.
+ *
+ * (c) Arash Ghamkhari <dev@ghamkhari.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Opt;
+
+trait OptTrait
+{
+    private $OPT;
+
+    public function setOpt(int $opt, $value)
+    {
+        $this->OPT[$opt] = $value;
+        return $this;
+    }
+
+    public function getOpt(int $opt)
+    {
+        if (!$this->isOpt($opt))
+            return null;
+
+        return $this->OPT[$opt];
+    }
+
+    public function isOpt(int $opt): bool
+    {
+        return isset($this->OPT[$opt]);
+    }
+}
